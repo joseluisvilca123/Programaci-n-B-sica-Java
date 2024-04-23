7f2fef2f9bd9a022c58d0b9413b75e0651077322
joseluisvilca123 <1540830@senati.pe>
2024-04-23 13:25:01 -0500

    first commit

---
+++ HolaMundo/src/holamundo/HolaMundo.java
@@ -0,0 +1,12 @@
+
+package holamundo;
+
+public class HolaMundo {
+
+
+    public static void main(String[] args) {
+        System.out.println("Jose Luis Vilca Salas");
+        System.out.println("Hola Mundo");
+    }
+    
+}
---
+++ tipodato1/src/tipodato1/TipoDato1.java
@@ -0,0 +1,31 @@
+package tipodato1;
+
+public class TipoDato1 {
+
+ 
+    public static void main(String[] args) {
+        // Cadena o String
+        String Nombre = "";
+        int Edad;
+        boolean Estado;
+        double Saldo;
+        Object Todotipodato;
+        String nombre = "";
+        //Asignado
+        Nombre = "Jose Luis Vilca Salas";
+        Edad = 20;
+        Saldo = 100.34;
+       
+        Todotipodato = "cadena";
+        Todotipodato = 21;
+        Todotipodato = true;
+        Todotipodato = 23.23;
+               
+        //impri
+        System.out.println("Nombre:" +  Nombre);
+        System.out.println("Edad:" + Edad);
+        System.out.println("Saldo:" + Saldo);
+        System.out.println("objet:" + Todotipodato);
+    }
+   
+}
